926b8bb195c4d2d6840b3d69624f983fc999fe71
Stefan923 <stefanpopescu923@gmail.com>
2020-08-17 16:22:50 +0300

    Added new options for translations.

--- src/main/java/me/stefan923/ultimateafk/language/LanguageManager.java
+++ src/main/java/me/stefan923/ultimateafk/language/LanguageManager.java
@@ -28,6 +28,8 @@ public class LanguageManager {
         config.addDefault("Command.SetLocation.Success", "&8(&3!&8) &fYou have set a &bnew location &fwhere to teleport afk players.");
         config.addDefault("Command.SetLocation.Not Enabled", "&8(&3!&8) &fTeleporting afk players is &bnot enabled &fin &3settings.yml&f.");
         config.addDefault("General.Afk Location Not Set", "&8(&3!&8) &cThe AFK location for &4UltimateAfk &cis not set!");
+        config.addDefault("General.Invalid Command Syntax", "&8(&3!&8) &cInvalid Syntax or you have no permission!\n&8(&3!&8) &fThe valid syntax is: &b%syntax%");
+        config.addDefault("General.No Permission", "&8(&3!&8) &cYou need the &4%permission% &cpermission to do that!");
         config.addDefault("General.You Are Afk", "&8(&3!&8) &fYou are &bafk &fnow! Use &3/exit &f or &3move &fto return to spawn.");
         config.addDefault("General.You Are Not Afk", "&8(&3!&8) &fYou did an action, so you are &bno longer afk&f!");
         config.options().copyDefaults(true);
@@ -39,12 +41,15 @@ public class LanguageManager {
     }
 
     public void resetConfig() {
-        config.set("Command.Exit.Success", "&8(&3!&8) &fYou have typed &3/exit&f. You are &bNOT afk &fanymore!");
         config.set("Command.Exit.Not Afk", "&8(&3!&8) &fYou are &bnot &fafk!");
-        config.set("Event.You Are Afk", "&8(&3!&8) &fYou are &bafk &fnow! Use &3/exit &f or &3move &fto return to spawn.");
-        config.set("Event.Move Event", "&8(&3!&8) &fYou have moved. You are &bNOT afk &fanymore!");
-        config.set("Event.Chat Event", "&8(&3!&8) &fYou have sent a message. You are &bNOT afk &fanymore!");
+        config.set("Command.Exit.Success", "&8(&3!&8) &fYou have typed &3/exit&f. You are &bNOT afk &fanymore!");
+        config.set("Command.SetLocation.Success", "&8(&3!&8) &fYou have set a &bnew location &fwhere to teleport afk players.");
+        config.set("Command.SetLocation.Not Enabled", "&8(&3!&8) &fTeleporting afk players is &bnot enabled &fin &3settings.yml&f.");
         config.set("General.Afk Location Not Set", "&8(&3!&8) &cThe AFK location for &4UltimateAfk &cis not set!");
+        config.set("General.Invalid Command Syntax", "&8(&3!&8) &cInvalid Syntax or you have no permission!\n&8(&3!&8) &fThe valid syntax is: &b%syntax%");
+        config.set("General.No Permission", "&8(&3!&8) &cYou need the &4%permission% &cpermission to do that!");
+        config.set("General.You Are Afk", "&8(&3!&8) &fYou are &bafk &fnow! Use &3/exit &f or &3move &fto return to spawn.");
+        config.set("General.You Are Not Afk", "&8(&3!&8) &fYou did an action, so you are &bno longer afk&f!");
         save();
     }
 
