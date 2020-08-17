8a91f075242cb88be313d5553e341456f9185c0d
Stefan923 <stefanpopescu923@gmail.com>
2020-08-18 00:16:22 +0300

    Clearing title packets after leaving the AFK status.

--- src/main/java/me/stefan923/ultimateafk/commands/type/CommandExit.java
+++ src/main/java/me/stefan923/ultimateafk/commands/type/CommandExit.java
@@ -2,24 +2,36 @@ package me.stefan923.ultimateafk.commands.type;
 
 import me.stefan923.ultimateafk.UltimateAfk;
 import me.stefan923.ultimateafk.commands.AbstractCommand;
+import me.stefan923.ultimateafk.nms.Title;
 import me.stefan923.ultimateafk.utils.MessageUtils;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public class CommandExit extends AbstractCommand implements MessageUtils {
 
+    private Title title;
     public CommandExit() {
         super(null, true, "exit");
+        title = new Title();
     }
 
     @Override
     protected ReturnType runCommand(UltimateAfk instance, CommandSender sender, String... args) {
-        String playerName = sender.getName();
+        Player player = (Player) sender;
+        String playerName = player.getName();
+        FileConfiguration settings = instance.getSettingsManager().getConfig();
 
         if (instance.getAfkPlayers().containsKey(playerName)) {
-            ((Player) sender).teleport(instance.getAfkPlayers().get(playerName));
+            if (settings.getBoolean("Afk Settings.Use Titles") && instance.getAfkPlayers().containsKey(player.getName())) {
+                title.send(player, "", "", 0, 1, 0);
+            }
+            if (settings.getBoolean("Afk Settings.Teleport Afk Players")) {
+                player.teleport(instance.getAfkPlayers().get(playerName));
+            }
+
             instance.getAfkPlayers().remove(playerName);
             sender.sendMessage(formatAll(instance.getLanguageManager().getConfig().getString("Command.Exit.Success")));
             return ReturnType.SUCCESS;
